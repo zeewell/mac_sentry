67c1ba29bc11afa26ddfcbbe4f66f51e64bdc2a8
Lakr <git@qaq.wiki>
2025-05-24 04:20:48 +0800

    Update ContentView.swift

--- Sentry/ContentView.swift
+++ Sentry/ContentView.swift
@@ -57,7 +57,7 @@ struct ContentView: View {
                             if vm.canActivate {
                                 titleType = .lockToContinue
                             } else {
-                                TitleType = .setupNow
+                                titleType = .setupNow
                             }
                         }
                     }
