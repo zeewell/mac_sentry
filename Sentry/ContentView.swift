0c59fdc40eaa152bb640d5893f136ea7feb5ab2c
Lakr <git@qaq.wiki>
2025-06-19 03:06:31 +0800

    fixed #4

--- Sentry/ContentView.swift
+++ Sentry/ContentView.swift
@@ -12,105 +12,124 @@ struct ContentView: View {
     @State var sentry: Sentry? = nil
     @State var hint: String = ""
 
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    func tik_welcome() {
+        guard vm.status == .welcome else { return }
+        guard sentry == nil else { return }
+        guard let check = DeviceCheck.isMacLocked(), check else { return }
+        let sentry: Sentry = .init(
+            configuration: SentryConfigurationManager.shared.cfg
+        ) { alarmingReason in
+            print("[*] alarming reason: \(alarmingReason)")
+            vm.status = .activityDetected
+            hint = String(localized: "An alarm was triggered at: \(Date().formatted()). Reason: \(alarmingReason)")
+        }
+        self.sentry = sentry
+        sentry.run()
+        vm.status = .running
+    }
+
+    func tik_running() {
+        guard vm.status == .running else { return }
+        guard let sentry else { return }
+        if DeviceCheck.isMacLocked() ?? true { return }
+        vm.status = .completed
+        sentry.stop()
+    }
+
+    func tik_activityDetected() {
+        guard vm.status == .activityDetected else { return }
+        guard let sentry else { return }
+        // if the mac is locked, continue to run
+        if DeviceCheck.isMacLocked() ?? true { return }
+        // unlocked, stop the sentry
+        sentry.stop()
+    }
 
     var body: some View {
-        switch vm.status {
-        case .welcome:
-            WelcomePanel()
-                .onReceive(timer) { _ in
-                    guard vm.status == .welcome else { return }
-                    guard sentry == nil else { return }
-                    guard let check = DeviceCheck.isMacLocked(), check else { return }
-                    let sentry: Sentry = .init(
-                        configuration: SentryConfigurationManager.shared.cfg
-                    ) { alarmingReason in
-                        print("[*] alarming reason: \(alarmingReason)")
-                        vm.status = .activityDetected
-                        hint = String(localized: "An alarm was triggered at: \(Date().formatted()). Reason: \(alarmingReason)")
-                    }
-                    self.sentry = sentry
-                    sentry.run()
-                    vm.status = .running
+        ZStack {
+            switch vm.status {
+            case .welcome:
+                WelcomePanel()
+            case .running:
+                VStack(spacing: 32) {
+                    EyeView()
                 }
-        case .running:
-            VStack(spacing: 32) {
-                EyeView()
-            }
-            .padding(64)
-            .onReceive(timer) { _ in
-                guard vm.status == .running else { return }
-                guard let sentry else { return }
-                if DeviceCheck.isMacLocked() ?? true { return }
-                vm.status = .completed
-                sentry.stop()
-            }
-        case .activityDetected:
-            VStack(spacing: 16) {
-                Image(systemName: "light.beacon.max")
-                    .font(.largeTitle)
-                Text("Activity Detected")
-                    .bold()
-                Divider()
-                Text(hint)
-                Divider()
-                HStack {
-                    if sentry?.configuration.sentryRecordingEnabled ?? false {
-                        Button("Open Saved Clips") {
-                            try? FileManager.default.createDirectory(
-                                atPath: videoClipDir.path,
-                                withIntermediateDirectories: true
-                            )
-                            NSWorkspace.shared.selectFile(
-                                nil,
-                                inFileViewerRootedAtPath: videoClipDir.path
-                            )
+                .padding(64)
+            case .activityDetected:
+                VStack(spacing: 16) {
+                    Image(systemName: "light.beacon.max")
+                        .font(.largeTitle)
+                    Text("Activity Detected")
+                        .bold()
+                    Divider()
+                    Text(hint)
+                    Divider()
+                    HStack {
+                        if sentry?.configuration.sentryRecordingEnabled ?? false {
+                            Button("Open Saved Clips") {
+                                try? FileManager.default.createDirectory(
+                                    atPath: videoClipDir.path,
+                                    withIntermediateDirectories: true
+                                )
+                                NSWorkspace.shared.selectFile(
+                                    nil,
+                                    inFileViewerRootedAtPath: videoClipDir.path
+                                )
+                            }
+                        }
+                        Button("Exit") {
+                            exit(0)
                         }
                     }
-                    Button("Exit") {
-                        exit(0)
-                    }
                 }
-            }
-            .padding(64)
-            .onReceive(timer) { _ in
-                guard vm.status == .activityDetected else { return }
-                guard let sentry else { return }
-                // if the mac is locked, continue to run
-                if DeviceCheck.isMacLocked() ?? true { return }
-                // unlocked, stop the sentry
-                sentry.stop()
-            }
-        case .completed:
-            VStack(spacing: 16) {
-                Image(systemName: "checkmark.circle.fill")
-                    .font(.largeTitle)
-                    .foregroundStyle(.green)
-                Text("Sentry Completed")
-                    .bold()
-                Divider()
-                HStack {
-                    if sentry?.configuration.sentryRecordingEnabled ?? false {
-                        Button("Open Saved Clips") {
-                            try? FileManager.default.createDirectory(
-                                atPath: videoClipDir.path,
-                                withIntermediateDirectories: true
-                            )
-                            // select the directory
-                            NSWorkspace.shared.selectFile(
-                                nil,
-                                inFileViewerRootedAtPath: videoClipDir.path
-                            )
+                .padding(64)
+            case .completed:
+                VStack(spacing: 16) {
+                    Image(systemName: "checkmark.circle.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.green)
+                    Text("Sentry Completed")
+                        .bold()
+                    Divider()
+                    HStack {
+                        if sentry?.configuration.sentryRecordingEnabled ?? false {
+                            Button("Open Saved Clips") {
+                                try? FileManager.default.createDirectory(
+                                    atPath: videoClipDir.path,
+                                    withIntermediateDirectories: true
+                                )
+                                // select the directory
+                                NSWorkspace.shared.selectFile(
+                                    nil,
+                                    inFileViewerRootedAtPath: videoClipDir.path
+                                )
+                            }
+                        }
+                        Button("Exit") {
+                            exit(0)
                         }
                     }
-                    Button("Exit") {
-                        exit(0)
-                    }
+                }
+                .padding(64)
+                .onAppear {
+                    sentry?.stop()
                 }
             }
-            .padding(64)
-            .onAppear {
-                sentry?.stop()
+        }
+        .onAppear {
+            vm.setTimerCallback {
+                print("[*] interface timer tik: \(vm.status)")
+                defer { print("[*] interface timer tik out: \(vm.status)") }
+                switch vm.status {
+                case .welcome:
+                    tik_welcome()
+                case .running:
+                    tik_running()
+                case .activityDetected:
+                    tik_activityDetected()
+                case .completed:
+                    break
+                }
             }
         }
     }
@@ -118,7 +137,12 @@ struct ContentView: View {
 
 class ViewModel: ObservableObject {
     static let shared = ViewModel()
-    private init() {}
+    private init() {
+        let timer = Timer(timeInterval: 1, repeats: true) { [weak self] _ in
+            self?.callback()
+        }
+        RunLoop.main.add(timer, forMode: .common)
+    }
 
     enum PanelStatus {
         case welcome
@@ -128,6 +152,12 @@ class ViewModel: ObservableObject {
     }
 
     @Published var status: PanelStatus = .welcome
+
+    private var callback = {}
+
+    func setTimerCallback(_ cb: @escaping () -> Void) {
+        callback = cb
+    }
 }
 
 #Preview {
